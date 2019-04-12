cee7fd05bd01d4b068a2601c617e5025fd5e8700
Joshua Sheridan <jooshkins@gmail.com>
2019-04-12 16:59:14 -0700

    Changed Status to use popovers to display log info

--- src/Components/ScriptStatus.js
+++ src/Components/ScriptStatus.js
@@ -1,29 +1,41 @@
 import React from 'react'
-import { Icon, Tooltip, Spinner } from "@blueprintjs/core";
+import { Icon, Popover, Spinner, Button } from "@blueprintjs/core";
 import '@blueprintjs/core/lib/css/blueprint.css';
 import './style.css'
 
 class ScriptStatus extends React.Component {
+    
     render() {
+    let content = <div className="toolTip"><pre>{this.props.content}</pre></div>
         if (this.props.mode === 'icon') {
             return (
-                <Tooltip content={this.props.content}>
-                    <Icon
-                        icon={this.props.icon}
-                        iconSize={20}
-                        intent={this.props.intent}
-                    />
-                </Tooltip>
+                <Popover content={content}>
+                    <Button
+                    minimal={true}
+                    intent={this.props.intent}
+                    >
+                        <Icon
+                            icon={this.props.icon}
+                            iconSize={20}
+                            intent={this.props.intent}
+                        />
+                    </Button>
+                </Popover>
             );
         }
         if (this.props.mode === 'spinner') {
             return (
-                <Tooltip content={this.props.content}>
-                    <Spinner
-                        small={true}
-                        className="m-10"
-                    />
-                </Tooltip>
+                <Popover content={content}>
+                    <Button
+                    minimal={true}
+                    intent={this.props.intent}
+                    >
+                        <Spinner
+                            small={true}
+                            className="m-10"
+                        />
+                    </Button>
+                </Popover>            
             );
         }
     }
